4ee1700c1f253f33639e50c727580c943e305810
matth747 <109696245+matth747@users.noreply.github.com>
2022-10-03 22:26:23 -0500

    added final changes before 1st submission

--- README.md
+++ README.md
@@ -2,27 +2,16 @@
 
 ## Description
 
-Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
-
-- What was your motivation?
-- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-- What problem does it solve?
-- What did you learn?
-
+This scheduler uses javascript to save text for your workday.  it still needs moment.js and I'm currently working on getting my local storage to work. 
 
 
 ## Usage
 
-Provide instructions and examples for use. Include screenshots as needed.
 
-To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative file path, add it to your README using the following syntax:
+![alt text](assets/images/CH5-screenshot.jpg)
 
-![alt text](assets/images/screenshot.png)
+website: https://matth747.github.io/workday_scheduler/
 
-## Credits
+github: https://github.com/matth747/workday_scheduler
 
-List your collaborators, if any, with links to their GitHub profiles.
 
-If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-
-If you followed tutorials, include links to those here as well.
--- assets/script.js
+++ assets/script.js
@@ -4,9 +4,43 @@ $('.btn').click(function () {
     //console.log($(this))
 
 
-    console.log($(this).parent().siblings('.col-10').attr('id'))
+    console.log($(this).siblings('.col-10').attr('id'))
 
-    var timeBlockVal = $(this).parent().siblings('.col-10').val()
+    var timeBlockVal = $(this).siblings('.col-10').val()
     console.log(timeBlockVal)
 
 })
+function saveInfo() {
+    // Save related form data as an object
+    var infoSaved = {
+      note: timeBlockVal
+      
+    };
+    // Use .setItem() to store object in storage and JSON.stringify to convert it as a string
+    localStorage.setItem("infoSaved", JSON.stringify(infoSaved));
+  }
+  function renderinfo() {
+    // Use JSON.parse() to convert text to JavaScript object
+    var todo = JSON.parse(localStorage.getItem("infoSaved"));
+    // Check if data is returned, if not exit out of the function
+    
+    document.getElementById('#morning-b8').innerHTML = infoSaved.note;
+    
+    }
+    $('.btn').click(saveInfo)
+    $('.btn').click(renderinfo)
+
+    var buttonClickHandler = function (event) {
+        // What is `event.target` referencing?
+        // TODO: Write your answer here
+        var language = event.target.getAttribute('data-language');
+      
+        // Why is this `if` block in place?
+        // TODO: Write your answer here
+        if (language) {
+          getFeaturedRepos(language);
+      
+          repoContainerEl.textContent = '';
+        }
+      };
+      
